79266878b3962a9ddff6e502e5a89afd712e8989
Harrison Ryan <hwryan12@gmail.com>
2023-05-17 18:41:48 -0500

    feature: Add group endpoint for docker test

--- lynk_up_server/lynk_up_server/urls.py
+++ lynk_up_server/lynk_up_server/urls.py
@@ -21,5 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', views.user_list),
-    path('users/<str:phone_number>', views.user_detail)
+    path('users/<str:phone_number>', views.user_detail),
+    path('groups/<int:group_id>/', views.group_detail, name='group_detail')
+
 ]
