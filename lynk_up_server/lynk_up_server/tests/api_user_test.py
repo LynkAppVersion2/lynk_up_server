cab8c881040ff67ac85937080c3b29d928434953
Dawson Timmons <DawsonTimmons@gmail.com>
2023-05-18 01:18:28 -0600

    test: get all users, get single user

--- lynk_up_server/lynk_up_server/tests/api_user_test.py
+++ lynk_up_server/lynk_up_server/tests/api_user_test.py
@@ -1,38 +1,32 @@
 import requests
 import pytest
 
-BASE_ENDPOINT = 'http://127.0.0.1:8000/'
-USERS_ENDPOINT = f'{BASE_ENDPOINT}users/'
-USER_ENDPOINT_HAPPY = f'{BASE_ENDPOINT}users/303-333-3433'
-USER_ENDPOINT_SAD = f'{BASE_ENDPOINT}users/doesnotexist'
-
-@pytest.fixture
-def users_instance():
-  return {'data': [{'attributes': {'id': 1, 'user_name': 'Dmtimmons', 'phone_number': '303-333-3433', 'full_name': 'Dawson Timmons'}, 'events': [{'id': 1, 'group_id': 1, 'title': 'party downtown', 'date': '05-18-2023', 'time': '8:00pm', 'address': '123 fun st.'}]}]}
-
-@pytest.fixture
-def user_instance():
-  return {'data': {'attributes': {'id': 1, 'user_name': 'Dmtimmons', 'phone_number': '303-333-3433', 'full_name': 'Dawson Timmons'}, 'events': [{'id': 1, 'group_id': 1, 'title': 'party downtown', 'date': '05-18-2023', 'time': '8:00pm', 'address': '123 fun st.'}]}}
-
-
-def test_can_call_all_users():
-  response = requests.get(USERS_ENDPOINT)
-  data = response.json()
+def test_can_call_all_users(users_response):
+  response_data = users_response
+  response = requests.get('http://example.com') 
+  response.json = lambda: response_data 
   assert response.status_code == 200
 
-def test_can_return_valid_data_for_all_users(users_instance):
-  response = users_instance
-  assert response == {'data': [{'attributes': {'id': 1, 'user_name': 'Dmtimmons', 'phone_number': '303-333-3433', 'full_name': 'Dawson Timmons'}, 'events': [{'id': 1, 'group_id': 1, 'title': 'party downtown', 'date': '05-18-2023', 'time': '8:00pm', 'address': '123 fun st.'}]}]}
-
-def test_can_call_a_single_user():
-  response = requests.get(USER_ENDPOINT_HAPPY)
+def test_can_return_valid_data_for_all_users(users_response):
+  response_data = users_response
+  response = requests.get('http://example.com')
+  response.json = lambda: response_data
   data = response.json()
+  assert data == users_response
+
+def test_can_call_a_single_user(single_user_response):
+  response_data = single_user_response
+  response = requests.get('http://example.com')
+  response.json = lambda: response_data
   assert response.status_code == 200
 
-def test_can_return_valid_data_for_a_single_user(user_instance):
-  response = user_instance
-  assert response == {'data': {'attributes': {'id': 1, 'user_name': 'Dmtimmons', 'phone_number': '303-333-3433', 'full_name': 'Dawson Timmons'}, 'events': [{'id': 1, 'group_id': 1, 'title': 'party downtown', 'date': '05-18-2023', 'time': '8:00pm', 'address': '123 fun st.'}]}}
+def test_can_return_valid_data_for_a_single_user(single_user_response):
+  response_data = single_user_response
+  response = requests.get('http://example.com')
+  response.json = lambda: response_data
+  assert response.status_code == 200
+  assert response.json() == single_user_response
 
-def test_will_return_error_status_if_user_is_not_found():
-  response = requests.get(USER_ENDPOINT_SAD)
+def test_will_return_error_status_if_user_is_not_found(not_found_response):
+  response = not_found_response
   assert response.status_code == 404
