5cc992fd7214719bd7789b31358d4e0494408fd2
Andra Helton <andrahelton@gmail.com>
2023-05-18 20:45:09 -0600

    Add changes to wsgi.py to resolve deployment issues

--- lynk_up_server/lynk_up_server/wsgi.py
+++ lynk_up_server/lynk_up_server/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from lynk_up_server.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lynk_up_server.settings')
 
--- lynk_up_server/manage.py
+++ lynk_up_server/manage.py
@@ -9,7 +9,6 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lynk_up_server.settings')
     try:
         from django.core.management import execute_from_command_line
-        from lynk_up_server.lynk_up_server import wsgi 
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
