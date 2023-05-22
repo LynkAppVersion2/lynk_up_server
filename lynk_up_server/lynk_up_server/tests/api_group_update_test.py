3f83f6386aa6bb378914d62a8c0cff1899eb2e06
Harrison Ryan <hwryan12@gmail.com>
2023-05-22 07:10:00 -0500

    test: Add sad path

--- lynk_up_server/lynk_up_server/tests/api_group_update_test.py
+++ lynk_up_server/lynk_up_server/tests/api_group_update_test.py
@@ -15,3 +15,16 @@ def test_can_update_group(groups_update_response):
     
     assert response.status_code == 200
     assert response.json() == groups_update_response
+
+@responses.activate
+def test_update_non_existing_group():
+    responses.add(responses.PUT, 'http://example.com/9999/update/', status=404)
+
+    data = {
+        "user": 1,
+        "friends": [2, 3],
+        "name": "Updated Test Group"
+    }
+    response = requests.put('http://example.com/9999/update/', json=data)
+    
+    assert response.status_code == 404
