80a3c03a5a82686011e520ec57c85031ee85564e
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-05-23 17:28:30 -0600

    Create: superuser for deployment from  serializer

--- lynk_up_server/lynk_up_server/serializers.py
+++ lynk_up_server/lynk_up_server/serializers.py
@@ -45,3 +45,13 @@ class FriendsListSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = '__all__'
+
+from django.contrib.auth.models import User
+if not User.objects.filter(is_superuser=True).first():
+    user = User.objects.create(
+        username = 'admin',
+        email = 'admin@mywebsite.com',
+        is_superuser = True
+    )
+    user.set_password('Pass123')
+    user.save()
