706ca62db1ded113ff65183839db6869e286a15e
Andra Helton <andrahelton@gmail.com>
2023-06-27 20:55:29 -0600

    Add in-progress GroupSerializer - adding friends to a group

--- lynk_up_server/lynk_up_server/serializers.py
+++ lynk_up_server/lynk_up_server/serializers.py
@@ -12,6 +12,7 @@ class EventSerializer(serializers.ModelSerializer):
     return obj.group.name
   
 class FriendsListSerializer(serializers.ModelSerializer):
+
   class Meta:
     model = User
     fields = '__all__'
@@ -23,6 +24,7 @@ class FriendsListSerializer(serializers.ModelSerializer):
       'full_name': instance.full_name,
       'phone_number': instance.phone_number
     }
+  
   
 class GroupsListSerializer(serializers.ModelSerializer):
   class Meta:
@@ -40,8 +42,8 @@ class EventsListSerializer(serializers.ModelSerializer):
     return obj.group.name
   
 class GroupSerializer(serializers.ModelSerializer):
-  friends_list = FriendsListSerializer(many=True)
-  events = EventsListSerializer(many=True)
+  friends_list = FriendsListSerializer(many=True, required=False)
+  events = EventsListSerializer(many=True, read_only=True)
 
   class Meta:
     model = Group
@@ -56,6 +58,84 @@ class GroupSerializer(serializers.ModelSerializer):
       'type': 'group',
       'attributes': attributes
     }
+  
+
+# --------------------------
+# LATEST V
+# --------------------------
+# class GroupSerializer(serializers.ModelSerializer):
+#   friends_list = serializers.SerializerMethodField()
+#   events = serializers.SerializerMethodField()
+
+#   class Meta:
+#       model = Group
+#       fields = ('id', 'user', 'name', 'friends_list', 'events')
+
+#   def to_representation(self, instance):
+#       import ipdb; ipdb.set_trace()
+#       ret = super().to_representation(instance)
+#       attributes = {'group_host_id': ret['user'], 'group_name': ret['name'], 'group_friends': ret['friends_list'], 'group_events': ret['events']}
+
+#       return {
+#           'id': ret['id'],
+#           'type': 'group',
+#           'attributes': attributes
+#       }
+
+#   def get_friends_list(self, instance):
+#       friends = instance.friends.values('friend_id', 'friend__full_name', 'friend__phone_number')
+#       modified_friends = []
+
+#       if self.context:
+#         friends_data = self.initial_data.get('friends_list', [])
+#         for friend_data in friends_data:
+#           friend_id = friend_data.get('friend_id')
+#           friend = User.objects.get(id=friend_id)
+#           modified_friend = {
+#             'friend_id': friend_id,
+#             'friend_name': friend.full_name,
+#             'phone_number': friend.phone_number
+#           }
+#           modified_friends.append(modified_friend)
+
+#       # if self.data:
+#       #   friends_data = self.initial_data.get('friends_list', [])
+#       #   for friend_data in friends_data:
+#       #       friend_id = friend_data.get('friend_id')
+#       #       friend = User.objects.get(id=friend_id)
+#       #       modified_friend = {
+#       #           'friend_id': friend_id,
+#       #           'friend_name': friend.full_name,
+#       #           'phone_number': friend.phone_number
+#       #       }
+#       #       modified_friends.append(modified_friend)
+
+#       for friend in friends:
+#         modified_friend = {
+#           'friend_id': friend['friend_id'],
+#           'friend_name': friend['friend__full_name'],
+#           'phone_number': friend['friend__phone_number']
+#         }
+#         modified_friends.append(modified_friend)
+#       return modified_friends
+
+#   def get_events(self, instance):
+#       events = instance.events.values('id', 'group_id', 'group__name', 'time', 'title')
+#       modified_events = []
+
+#       for event in events:
+#         modified_event = {
+#           'event_id': event['id'],
+#           'group_id': event['group_id'],
+#           'group_name': event['group__name'],
+#           'title': event['title'],
+#           'time': event['time']
+#         }
+#         modified_events.append(modified_event)
+#       return modified_events
+# --------------------------
+# LATEST ^
+# --------------------------
 
 class UserSerializer(serializers.ModelSerializer):
   events = EventsListSerializer(many=True)
