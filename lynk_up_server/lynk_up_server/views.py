b79d423ca8802f451c32a5fe371ef60e609605ee
Andra Helton <andrahelton@gmail.com>
2023-06-27 20:40:52 -0600

    fix: Batched some requests into a single method

--- lynk_up_server/lynk_up_server/views.py
+++ lynk_up_server/lynk_up_server/views.py
@@ -6,16 +6,24 @@ from rest_framework import status
 from django.db import IntegrityError
 from django.views.decorators.http import require_http_methods
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def user_list(request):
 
   if request.method == 'GET':
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response({"data": serializer.data})
+  
+  elif request.method == 'POST':
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT' 'DELETE'])
 def user_detail(request, user_id):
   try:
     user = User.objects.get(pk=user_id)
@@ -25,57 +33,56 @@ def user_detail(request, user_id):
   if request.method == 'GET':
     serializer = UserSerializer(user)
     return Response({"data": serializer.data})
+  
+  elif request.method == 'PUT':
+    serializer = UserSerializer(user, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  elif request.method == 'DELETE':
+    user.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+  
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def group_list(request):
   if request.method == 'GET':
     groups = Group.objects.all()
     serializer = GroupSerializer(groups, many=True)
     return Response({"data": serializer.data})
-
-@api_view(['GET'])
-def group_detail(request, group_id):
-  try:
-      group = Group.objects.get(pk=group_id)
-  except Group.DoesNotExist:
-      return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
-      serializer = GroupSerializer(group)
-      return Response(serializer.data)
-
-@api_view(['POST'])
-def group_create(request):
-  serializer = GroupSerializer(data=request.data)
-  if serializer.is_valid():
+  
+  elif request.method == 'POST':
+    serializer = GroupSerializer(data=request.data, context={'request_method': 'PUT'})
+    if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
-  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
 
-@api_view(['PUT'])
-def group_update(request, group_id):
+@api_view(['GET', 'PUT', 'DELETE'])
+def group_detail(request, group_id):
   try:
     group = Group.objects.get(pk=group_id)
   except Group.DoesNotExist:
     return Response(status=status.HTTP_404_NOT_FOUND)
 
-  if request.method == 'PUT':
-    serializer = GroupSerializer(group, data=request.data)
+  if request.method == 'GET':
+    serializer = GroupSerializer(group)
+    return Response({"data": serializer.data})
+  
+  elif request.method == 'PUT':
+    serializer = GroupSerializer(group, data=request.data, context={'request_method': 'PUT'})
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  elif request.method == 'DELETE':
+    group.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['DELETE'])
-def group_delete(request, group_id):
-    try:
-        group = Group.objects.get(pk=group_id)
-    except Group.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'DELETE':
-        group.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def event_list(request):
@@ -89,6 +96,7 @@ def event_list(request):
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def event_detail(request, event_id):
@@ -113,7 +121,7 @@ def event_detail(request, event_id):
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['POST', 'GET', 'DELETE'])
+@api_view(['GET', 'POST'])
 def friends(request, user_id):
   if request.method == 'GET':
     user = User.objects.get(id=user_id)
@@ -125,7 +133,7 @@ def friends(request, user_id):
   elif request.method == 'POST':
     try:
       user = User.objects.get(id=user_id)
-      friend = User.objects.get(id=request.data['friend_id'])
+      friend = User.objects.get(id=request.data['friend'])
 
       if friend not in user.added_friends():
         Friend.objects.create(user=user, friend=friend)
@@ -139,19 +147,24 @@ def friends(request, user_id):
       {"data": {"friends":serializer.data}}, status=201, content_type='application/json'
     )
 
+
+@api_view(['GET', 'DELETE'])
+def friend_detail(request, user_id, friend_id):
+
+  if request.method == 'GET':
+    user = User.objects.get(id=user_id)
+    friend = User.objects.get(id=friend_id)
+
+    serializer = UserSerializer(friend)
+    return Response({"data": serializer.data})
+
   elif request.method == 'DELETE':
-    try:
-      user = User.objects.get(id=user_id)
-      friend_id = int(request.data['friend_id'])
-
-      friend = User.objects.get(id=friend_id)
-      friendship = Friend.objects.filter(user=user, friend=friend).first()
-
-      if friendship:
-        friendship.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-      else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    except User.DoesNotExist:
+    import ipdb; ipdb.set_trace()
+    user = User.objects.get(id=user_id)
+    friend = User.objects.get(id=friend_id)
+    friendship = Friend.objects.filter(user=user, friend=friend).first()
+    if friendship:
+      friendship.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
       return Response(status=status.HTTP_404_NOT_FOUND)
